fda6fbdd3ba0a41e010f1e570a3efaf35bb9c119
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-10-24 12:51:43 +0200

    create quality section

---
+++ src/components/quality.js
@@ -0,0 +1,21 @@
+import QualityImg from "../assets/about/mobile/image-quality.jpg";
+import "../scss/layout/_quality.scss";
+
+export const Quality = () => {
+  return (
+    <section className="main">
+      <img src={QualityImg} className="quality_img" alt="quality"></img>
+      <div className="quality_content">
+        <h3 className="quality_title">Uncompromising quality</h3>
+        <p className="quality_description">
+          Although we work with growers who pay close attention to all stages of
+          harvest and processing, we employ, on our end, a rigorous quality
+          control program to avoid over-roasting or baking the coffee dry. Every
+          bag of coffee is tagged with a roast date and batch number. Our goal
+          is to roast consistent, user-friendly coffee, so that brewing is easy
+          and enjoyable.
+        </p>
+      </div>
+    </section>
+  );
+};
--- src/pages/about/mainAbout.js
+++ src/pages/about/mainAbout.js
@@ -1,10 +1,12 @@
 import { Commitment } from "../../components/commitment";
+import { Quality } from "../../components/quality";
 
 export const MainAbout = () => {
   return (
     <>
       <main>
         <Commitment />
+        <Quality />
       </main>
     </>
   );
