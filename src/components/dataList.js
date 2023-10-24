31be6884c78d546d5a436a65c787c4ef29d7041b
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-10-24 16:55:10 +0200

    location section

--- src/components/dataList.js
+++ src/components/dataList.js
@@ -7,6 +7,10 @@ import quality from "../assets/home/desktop/icon-coffee-bean.svg";
 import gifts from "../assets/home/desktop/icon-gift.svg";
 import truck from "../assets/home/desktop/icon-truck.svg";
 
+import uk from "../assets/about/desktop/illustration-uk.svg";
+import canada from "../assets/about/desktop/illustration-canada.svg";
+import australia from "../assets/about/desktop/illustration-australia.svg";
+
 export const Collections = [
   {
     id: "01",
@@ -90,5 +94,44 @@ export const WorksData = [
     title: "Receive and enjoy!",
     summary:
       "We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.",
+  },
+];
+
+export const LocationData = [
+  {
+    id: "30",
+    locMap: uk,
+    country: "United Kingdom",
+    adress: {
+      street: "68 Asfordby Rd",
+      town: "Alcaston",
+      code: "SY6 1YA",
+      phone: "+44 1241 918425",
+    },
+    alt: "our location in the united state",
+  },
+  {
+    id: "31",
+    locMap: canada,
+    country: "Canada",
+    adress: {
+      street: "1528 Eglinton Avenue",
+      town: "Toronto",
+      code: "Ontario M4P 1A6",
+      phone: "+1 416 485 2997",
+    },
+    alt: "our location in the canada",
+  },
+  {
+    id: "32",
+    locMap: australia,
+    country: "Australia",
+    adress: {
+      street: "36 Swanston Street",
+      town: "Kewell",
+      code: "Victoria",
+      phone: "+61 4 9928 3629",
+    },
+    alt: "our location in the australia",
   },
 ];
--- src/pages/about/mainAbout.js
+++ src/pages/about/mainAbout.js
@@ -1,13 +1,13 @@
 import { Commitment } from "../../components/commitment";
+import { LocationList } from "../../components/location";
 import { Quality } from "../../components/quality";
 
 export const MainAbout = () => {
   return (
-    <>
-      <main>
-        <Commitment />
-        <Quality />
-      </main>
-    </>
+    <main>
+      <Commitment />
+      <Quality />
+      <LocationList />
+    </main>
   );
 };
