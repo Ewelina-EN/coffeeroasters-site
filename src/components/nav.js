ef9791456c453dab76990b1b479318885dfa02c3
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-11-08 19:02:34 +0100

    nav for tablets

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
@@ -9,9 +9,23 @@ import "../scss/layout/_nav.scss";
 
 export const Navigation = ({ isFooter }) => {
   const [sidebar, setSidebar] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   const navListClassName = isFooter ? "footer-menu" : "nav-menu";
 
   const showSidebar = () => setSidebar(!sidebar);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
 
   return (
     <>
@@ -22,20 +36,22 @@ export const Navigation = ({ isFooter }) => {
           </Link>
           {/* add to FaBars css on hover cursor style(pointer etc)
           change size of hamburger icon for better UI on touch */}
-          <FaBars onClick={showSidebar} />
+          {windowWidth <= 640 ? (
+            <FaBars onClick={showSidebar} />
+          ) : (
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+              <ul className="nav-menu-items" onClick={showSidebar}>
+                <li className="navbar-toggle">
+                  <Link to="/">
+                    <img src={LogoNav} className="navbar-logo" alt="logo" />
+                  </Link>
+                  <AiOutlineClose />
+                </li>
+                <NavList className={navListClassName} />
+              </ul>
+            </nav>
+          )}
         </div>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link to="/">
-                <img src={LogoNav} className="navbar-logo" alt="logo"></img>
-              </Link>
-              {/* the same like in 23 line */}
-              <AiOutlineClose />
-            </li>
-            <NavList className={navListClassName} />
-          </ul>
-        </nav>
       </IconContext.Provider>
     </>
   );
--- src/components/navList.js
+++ src/components/navList.js
@@ -4,15 +4,13 @@ import { SidebarData } from "../components/sidebardata";
 export const NavList = () => {
   return (
     <>
-      <ul>
-        {SidebarData.map((item, index) => {
-          return (
-            <li key={index} className={item.cName}>
-              <Link to={item.path}>{item.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      {SidebarData.map((item, index) => {
+        return (
+          <li key={index} className={item.cName}>
+            <Link to={item.path}>{item.title}</Link>
+          </li>
+        );
+      })}
     </>
   );
 };
