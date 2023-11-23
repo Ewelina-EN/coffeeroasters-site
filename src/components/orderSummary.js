c6fddab2a1dc6a42879fa3c35037497e7d32ffab
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-11-23 18:54:00 +0100

    styles for order

--- src/components/orderSummary.js
+++ src/components/orderSummary.js
@@ -7,14 +7,18 @@ export const OrderSummary = ({ selectedOptions, toggleOrderCheckout }) => {
         <h3 className="summary_title">ORDER SUMMARY</h3>
         <p className="summary_description">
           “I drink my coffee as{" "}
-          <span>{selectedOptions.preference || "____"}</span>, with a{" "}
-          <span>{selectedOptions.bean || "____"}</span> type of bean.
+          <span>{selectedOptions.preference || "____"}</span>, with a
+          <span> {selectedOptions.bean || "____"}</span> type of bean.
           <span> {selectedOptions.quantity || "____"}</span> ground ala
           <span> {selectedOptions.grind || "____"}</span>, sent to me
           <span> {selectedOptions.deliveries || "____"}</span>.”
         </p>
       </div>
-      <button type="submit" onClick={toggleOrderCheckout}>
+      <button
+        type="submit"
+        onClick={toggleOrderCheckout}
+        className="header_btn summary_btn"
+      >
         Create my plan!
       </button>
     </form>
