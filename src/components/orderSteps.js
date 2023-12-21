5392c7303f865a649fb2e8ee2a589e4129b829bc
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-12-21 18:38:23 +0100

    styles for orderSteps on tablets

--- src/components/orderSteps.js
+++ src/components/orderSteps.js
@@ -3,14 +3,23 @@ import "../scss/layout/_orderSteps.scss";
 
 export const OrderSteps = () => {
   return (
-    <section className="order">
-      <ul className="order_list">
+    <section className="order order-dark">
+      <hr className="dark-line" />
+      <section className="dark-point">
+        <div className="dark-circle"></div>
+        <div className="dark-circle"></div>
+        <div className="dark-circle"></div>
+      </section>
+
+      <ul className="order_list dark_list">
         {OrderStepsList.map((step) => {
           return (
-            <li key={step.id} className="order_item">
-              <h2 className="order_num">{step.num}</h2>
-              <h3 className="order_title">{step.title}</h3>
-              <p className="order_description">{step.summary}</p>
+            <li key={step.id} className="order_item dark_item">
+              <h2 className="order_num dark_num">{step.num}</h2>
+              <h3 className="order_title dark_title">{step.title}</h3>
+              <p className="order_description dark_description">
+                {step.summary}
+              </p>
             </li>
           );
         })}
